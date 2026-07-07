ffa379222ab2afc8db7c436ed95f559e39c6e57d
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-07 22:54:03 +0900

    Add public version header

---
+++ include/VarjoToolkit/Version.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#define VARJOTOOLKIT_VERSION_MAJOR 0
+#define VARJOTOOLKIT_VERSION_MINOR 1
+#define VARJOTOOLKIT_VERSION_PATCH 0
+#define VARJOTOOLKIT_VERSION_STRING "0.1.0"
+
+namespace VarjoToolkit {
+
+inline constexpr int VersionMajor = VARJOTOOLKIT_VERSION_MAJOR;
+inline constexpr int VersionMinor = VARJOTOOLKIT_VERSION_MINOR;
+inline constexpr int VersionPatch = VARJOTOOLKIT_VERSION_PATCH;
+inline constexpr const char* VersionString = VARJOTOOLKIT_VERSION_STRING;
+
+} // namespace VarjoToolkit
